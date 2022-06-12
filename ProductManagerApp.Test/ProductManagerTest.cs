7d06084ad0e6f8eb74926d28249583ea21627769
lalanthir <lalanthir@yahoo.com>
2022-06-12 10:07:14 -0400

    Added unit test

--- ProductManagerApp.Test/ProductManagerTest.cs
+++ ProductManagerApp.Test/ProductManagerTest.cs
@@ -1,4 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using ProductApp.Data;
+using ProductApp.Domain;
 
 namespace ProductManagerApp.Test
 {
@@ -6,8 +9,22 @@ namespace ProductManagerApp.Test
     public class ProductManagerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddProduct()
         {
+            var builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase("AddProductDB");
+            using (var context = new ProductContext())
+            {
+                var product = new Product()
+                {
+                    Name = "Test Product",
+                    Type = "Books",
+                    Active = true,
+                    Price = 25.5M
+                };
+                context.Products.Add(product);
+                Assert.AreEqual(EntityState.Added, context.Entry(product).State);
+            }
         }
     }
 }
